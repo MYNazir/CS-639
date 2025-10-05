e2bbf4e0a6d382a2619a5082094e4ca649688730
Mohammed <naziryusif8@gmail.com>
2025-10-05 07:44:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/FirstFragment.java
@@ -0,0 +1,52 @@
+package com.example.aboutme;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.example.aboutme.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonHobbies.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+
+        binding.buttonContact.setOnClickListener(v -> {
+            Toast.makeText(getContext(), getString(R.string.going_to_contact), Toast.LENGTH_SHORT).show();
+            NavHostFragment.findNavController(FirstFragment.this)
+                    .navigate(R.id.action_FirstFragment_to_ThirdFragment);
+        });
+
+        binding.fab.setOnClickListener(v ->
+                Toast.makeText(getContext(), getString(R.string.fab_message), Toast.LENGTH_SHORT).show()
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
